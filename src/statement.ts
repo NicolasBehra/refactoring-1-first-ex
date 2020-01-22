b0ed192e620608bf2f670cc783bdd807ce611ac7
Nicolas Behra <behra.nicolas.romain@gmail.com>
2020-01-22 23:54:11 +0100

    amountFor() - Renamming "perf" to "aPerformance"

--- src/statement.ts
+++ src/statement.ts
@@ -11,7 +11,7 @@ export function statement(invoice, plays) {
   
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = amountFor(play, perf);
+    let thisAmount = amountFor(perf, play);
     
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -29,21 +29,21 @@ export function statement(invoice, plays) {
   return result;
 }
 
-function amountFor(play, perf): number {
+function amountFor(aPerformance, play): number {
   let result = 0;
   switch (play.type) {
     case "tragedy":
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
