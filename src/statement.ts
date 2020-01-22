46f0216f43ac693ea3828ab891a740b78232bf6a
Nicolas Behra <behra.nicolas.romain@gmail.com>
2020-01-23 00:29:04 +0100

    include functions "playFor()" & "amountFor()" into "statement()" tlangage typéo match the book

--- src/statement.ts
+++ src/statement.ts
@@ -9,17 +9,16 @@ export function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-
-    const play = playFor(perf, plays);
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
@@ -27,30 +26,30 @@ export function statement(invoice, plays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
-}
 
-function amountFor(aPerformance, play): number {
-  let result = 0;
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
+  function amountFor(aPerformance, play): number {
+    let result = 0;
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    return result;
   }
-  return result;
-}
-
-function playFor(aPerformance, plays) {
-  return plays[aPerformance.playID];
+  
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 }
