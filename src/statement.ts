5b84b642ae8a30e62bf955bf273c27ef520253be
Nicolas Behra <behra.nicolas.romain@gmail.com>
2020-02-01 15:48:24 +0100

    devided loop - totalAmount

--- src/statement.ts
+++ src/statement.ts
@@ -3,11 +3,13 @@ export function statement(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   
   for (let perf of invoice.performances) {
+    totalAmount += amountFor(perf);
+  }
+  for (let perf of invoice.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
-    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
