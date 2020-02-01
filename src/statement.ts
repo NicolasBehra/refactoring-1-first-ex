0c4540966ef491ba540544328544e73a34bb24df
Nicolas Behra <behra.nicolas.romain@gmail.com>
2020-02-01 16:48:22 +0100

    moved function - totalVolumeCredits()

--- src/statement.ts
+++ src/statement.ts
@@ -4,6 +4,7 @@ export function statement(invoice, plays) {
   statementData['performances'] = invoice.performances
   .map(enrichPerformance);
   statementData['totalAmount'] = totalAmount(statementData);
+  statementData['totalVolumeCredits'] = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -56,6 +57,14 @@ export function statement(invoice, plays) {
     return result;
   }
 
+  function totalVolumeCredits(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
+    }
+    return result;
+  }
+
 }
 
 function renderPlainText(data, plays) {
@@ -67,16 +76,8 @@ function renderPlainText(data, plays) {
     } seats)\n`;
   }
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
-  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
